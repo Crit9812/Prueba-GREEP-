af1622beebd8e8e111ae2f222313503315af1fe3
Christopher Octavio Téllez Domínguez <157320045+Crit9812@users.noreply.github.com>
2026-01-06 11:24:07 -0600

    Agregar import faltante para TableRow

--- Prueba/src/Operaciones/venta/controller/MainController.java
+++ Prueba/src/Operaciones/venta/controller/MainController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
