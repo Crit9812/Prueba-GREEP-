f8ee15d48ff2713691a9e32637ec6c992e3f5608
Christopher Octavio Téllez Domínguez <157320045+Crit9812@users.noreply.github.com>
2026-01-16 09:06:08 -0600

    Refrescar tabla tras crear productos

--- Prueba/src/Consultas/producto/controller/MainController.java
+++ Prueba/src/Consultas/producto/controller/MainController.java
@@ -326,16 +326,13 @@ public class MainController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Formularios/view/nuevoProducto.fxml"));
             Parent root = loader.load();
             controllerNuevoProducto ctrl = loader.getController();
-            ctrl.setOnSaved(this::preloadDatosUltraRapido);
+            ctrl.setOnSaved(this::actualizarProductos);
             Stage stage = new Stage();
             stage.initOwner(contenidoTabla.getScene().getWindow());
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setTitle("Nuevo producto");
             stage.setScene(new Scene(root));
             stage.showAndWait();
-            if (ctrl.isProductoCreado()) {
-                preloadDatosUltraRapido();
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
