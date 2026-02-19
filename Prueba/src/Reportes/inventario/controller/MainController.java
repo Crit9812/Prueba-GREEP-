7f31b53cfb3a6bca67e759cfcd4793e4c65a0e73
Christopher Octavio Téllez Domínguez <157320045+Crit9812@users.noreply.github.com>
2026-02-19 08:58:32 -0600

    Permite inventario detallado a usuarios y restringe edición por doble clic

--- Prueba/src/Reportes/inventario/controller/MainController.java
+++ Prueba/src/Reportes/inventario/controller/MainController.java
@@ -132,11 +132,6 @@ public class MainController implements ControladorVista{
                 }
             });
 
-            if (soloLectura && chkInventarioDetallado != null) {
-                chkInventarioDetallado.setSelected(false);
-                chkInventarioDetallado.setDisable(true);
-            }
-
             configurarColumnasTabla();
             configurarInventarioDetallado();
             configurarFiltros();
